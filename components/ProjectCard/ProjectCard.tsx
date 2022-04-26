db883061e1d0918d17d638becd3a5bfffd13337c
Christy Presler <cpresler@users.noreply.github.com>
2022-04-25 22:00:23 -0500

    feat: style project cards, create ProjectCard and GridContainer components

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -3,12 +3,13 @@ import styles from './ProjectCard.module.scss'
 import Image from 'next/image'
 import Link from '../Link/Link'
 import Text from '../Text/Text'
+import Button from '../Button/Button'
 
 interface Props {
   className?: string
-  slug?: string
-  image?: string
-  title?: string
+  slug: string
+  image: string
+  title: string
   desc?: string
 }
 
@@ -32,9 +33,10 @@ const ProjectCard = ({ className, slug, image, title, desc = '' }: Props) => {
           className={styles.card_image}
         />
         <div className={styles.card_details}>
-          <Text tag='h1' className={styles.card_title}>{ title }</Text>
+          <Text tag='h1' weight='medium' size='m' line='m' className={styles.card_title}>{ title }</Text>
           <Text className={styles.card_desc}>{ desc }</Text>
         </div>
+        <Button fullWidth={true} className={styles.card_button}>View Project</Button>
       </Link>
     </div>
   )
