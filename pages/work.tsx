48258a9adeb63fe72f4c33a2c75b882924aaad1d
Christy Presler <cpresler@users.noreply.github.com>
2022-04-25 22:12:12 -0500

    feat: add project card display to work page

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,21 +1,55 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import Construction from '../components/Construction/Construction'
+import fs from 'fs'
+import matter from 'gray-matter'
+import GridContainer from '../components/GridContainer/GridContainer'
+import ProjectCard from '../components/ProjectCard/ProjectCard'
+import Text from '../components/Text/Text'
 import styles from './Work.module.scss'
 
-const Work: NextPage = () => {
+export async function getStaticProps() {
+  // get projects
+  const files = fs.readdirSync('projects')
+  
+  const projects = files.map((fileName) => {
+    const slug = fileName.replace('.md', '')
+    const readFile = fs.readFileSync(`projects/${fileName}`, 'utf-8')
+    const { data: frontmatter } = matter(readFile)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  return {
+    props: {
+      projects,
+    },
+  }
+}
+
+const Work: NextPage = ({ projects }) => {
   return (
-    <>
+    <section>
       <Head>
         <meta name='description' content='Christy Presler - examples of past projects' />
       </Head>
+      <div className={styles.content}>
+        <Text tag='h1'>Selected Work</Text>
 
-      <section >
-        <Construction />
-      </section>
-    </>
+        <GridContainer>
+          {projects.map(({ slug, frontmatter }) => (
+            <ProjectCard
+              slug={slug}
+              image={frontmatter.thumbnail}
+              title={frontmatter.title}
+              desc={frontmatter.desc}
+            />
+          ))}
+        </GridContainer>
+      </div>
+    </section>
   )
 }
 
