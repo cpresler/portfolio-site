53b48343369d324f788ea0ca6fefbaf3a5ac07a2
Christy Presler <cpresler@users.noreply.github.com>
2022-05-03 12:32:37 -0500

    feat: convert about page to mdx and write first draft of content

--- layouts/AppLayout/AppLayout.tsx
+++ layouts/AppLayout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames"
 import styles from './AppLayout.module.scss'
-import Head from 'next/head'
+import MetaData from '../../components/MetaData/MetaData'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import { useRouter } from 'next/router'
@@ -23,12 +23,7 @@ const AppLayout = ({ children, className }: Props) => {
 
   return (
     <div className={classes}>
-      <Head>
-        <title>Christy Presler - Senior Design Technologist</title>
-        <meta name='description' content='Senior Design Technologist/UX Engineer with an insatiable curiosity about human behavior and a deep understanding of technology. I am a designer who implements engineering best practices and an engineer who delivers impeccable user experiences.' />
-        {/* TODO: replace favicon */}
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      <MetaData />
 
       <Header dark={dark}/>
 
