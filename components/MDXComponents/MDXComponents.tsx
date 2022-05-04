5e2d740ad7cbc5d6d83d763e66f6162f07109b39
Christy Presler <cpresler@users.noreply.github.com>
2022-05-04 16:06:33 -0500

    feat: add initial project content for web3 dapp and enterprise software

--- components/MDXComponents/MDXComponents.tsx
+++ components/MDXComponents/MDXComponents.tsx
@@ -3,16 +3,18 @@ import Link from '../Link/Link'
 import Project from '../../layouts/Project/Project'
 import MetaData from '../MetaData/MetaData'
 import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
+import Text from '../Text/Text'
 
 
 // assign custom components/classes to markdown elements
 const MDXComponents = {
   p: (props) => <p {...props} className={styles.p} />,
   a: (props) => <Link {...props} className={styles.link} />,
-  h1: (props) => <h1 {...props} className={styles.postTitle} />,
+  h1: (props) => <Text tag='h1' {...props} className={styles.pageTitle} />,
   Project: (props) => <Project {...props} className={styles.project} />,
   MetaData: (props) => <MetaData {...props} />,
   ResponsiveImage: (props) => <ResponsiveImage {...props} className={styles.img} />,
+  Text: (props) => <Text {...props} className={styles.featured} />
 };
 
 export default MDXComponents;
