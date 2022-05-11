3c2e219e7a521abf3202793e17b2b28576f5c1b7
Christy Presler <cpresler@users.noreply.github.com>
2022-05-11 11:01:31 -0500

    feat: finish first drafts of project write ups

--- components/MDXComponents/MDXComponents.tsx
+++ components/MDXComponents/MDXComponents.tsx
@@ -1,5 +1,5 @@
 import styles from './MDXComponents.module.scss';
-import Link from '../Link/Link'
+import Link, { NavLink } from '../Link/Link'
 import Project from '../../layouts/Project/Project'
 import MetaData from '../MetaData/MetaData'
 import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
@@ -11,10 +11,12 @@ const MDXComponents = {
   p: (props: any) => <p {...props} className={styles.p} />,
   a: (props: any) => <Link {...props} className={styles.link} />,
   h1: (props: any) => <Text tag='h1' {...props} className={styles.pageTitle} />,
+  h2: (props: any) => <Text tag='h2' {...props} className={styles.sectionTitle} />,
   Project: (props: any) => <Project {...props} className={styles.project} />,
   MetaData: (props: any) => <MetaData {...props} />,
   ResponsiveImage: (props: any) => <ResponsiveImage {...props} className={styles.img} />,
-  Text: (props: any) => <Text {...props} className={styles.featured} />
+  Text: (props: any) => <Text {...props} className={styles.featured} />,
+  Label: (props: any) => <Text {...props} className={styles.label} />
 };
 
 export default MDXComponents;
